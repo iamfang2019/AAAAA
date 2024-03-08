a2845a79afa8aeaa537cd5ff67d4308f48d5f901
frxz751113 <frxz751113@noreply.gitcode.com>
2024-03-08 21:36:33 +0800

    update: 更新文件 奇艺视频.js

--- js/奇艺视频.js
+++ js/奇艺视频.js
@@ -18,7 +18,7 @@ var rule = {
     },
     timeout:5000,
     class_name:'电影&电视剧&舞蹈&内地',
-    class_url:'1&2&5&42',
+    class_url:'1&2&5&221',
     limit:20,
     // play_parse:true,
     // 手动调用解析请求json的url,此lazy不方便
