d9177e3f0598791701640745f224f89988ea293b
frxz751113 <frxz751113@noreply.gitcode.com>
2024-06-17 11:31:42 +0800

    update: 更新文件 腾讯视频.js

--- js/腾讯视频.js
+++ js/腾讯视频.js
@@ -20,7 +20,7 @@ var rule = {
     timeout:5000,
     // class_parse:'.site_channel a;a&&Text;a&&href;channel/(.*)',
     cate_exclude:'会员|游戏|全部',
-    class_name:'免费热剧&最新&电视剧&电影&综艺&动漫',
+    class_name:'免费热剧&古装&电视剧&电影&综艺&武侠',
     class_url:'tv&movie&variety&cartoon',
     limit:20,
     // play_parse:true,
