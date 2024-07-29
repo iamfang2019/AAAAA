5fbb9d7106b401dc3745d076ef8c2b9bc50d0430
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-30 07:18:14 +0800

    Update V4检测.py

--- IPTV/V4检测.py
+++ IPTV/V4检测.py
@@ -13,7 +13,7 @@ open('TW.txt','wb').write(r.content)         #打开源文件并临时写入
 
 def test_connectivity(url):
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(url, timeout=30)
         return response.status_code == 200
     except requests.RequestException:
         return False
