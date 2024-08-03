920d8858ed96a4034316e92016bae703b60762ce
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-04 02:44:30 +0800

    Update 汇总检测.py

--- IPTV/汇总检测.py
+++ IPTV/汇总检测.py
@@ -49,9 +49,9 @@ def test_connectivity(url, max_attempts=1):
     # 尝试连接指定次数    
    for _ in range(max_attempts):  
     try:
-        response = requests.get(url, timeout=3)
-        #response = requests.head(url, timeout=3)  # 发送HEAD请求，仅支持V4
-        return 200 <= response.status_code < 300
+        #response = requests.get(url, timeout=3)
+        response = requests.head(url, timeout=3)  # 发送HEAD请求，仅支持V4
+        return 200 <= response.status_code < 400
         #return response.status_code == 200  # 返回True如果状态码为200
     except requests.RequestException:  # 捕获requests引发的异常
         pass  # 发生异常时忽略
