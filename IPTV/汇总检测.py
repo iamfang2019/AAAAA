99a0e5ae10b69a8ef1faefe1ae3b605504e847c3
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-03 18:34:16 +0800

    Update 汇总检测.py

--- IPTV/汇总检测.py
+++ IPTV/汇总检测.py
@@ -247,7 +247,7 @@ for line in lines:
 with open('汇总.txt', 'w', encoding="utf-8") as file:
  file.writelines(unique_lines)
 ################################################################################################任务结束，删除不必要的过程文件
-files_to_remove = ['去重.txt', "2.txt", "e.txt", "a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "c1.txt", "c2.txt", "d.txt", "f.txt", "f1.txt", "o1.txt", "o.txt", "2.txt"]
+files_to_remove = ['去重.txt', "2.txt", "e.txt", "a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "c1.txt", "c2.txt", "d.txt", "f0.txt", "f.txt", "f1.txt", "o1.txt", "o.txt", "2.txt"]
 for file in files_to_remove:
     if os.path.exists(file):
         os.remove(file)
